7e4ab32620ffd26d1dd069c9a6d1fda7d92447bb
vsheviakhova <vsheviakhova@gmail.com>
2022-06-15 10:57:18 +0300

    draft 1

--- 6_3.py
+++ 6_3.py
@@ -1,4 +1,7 @@
-a = 'hello world'
-b = 456
-print(type(a) == int)
-print(type(b) == int)
+class Nicola:
+    def __init__(self, age, name='Николай'):
+        self.age = age
+        if name != 'Николай':
+            self.name = f'Я не {name}, а Николай'
+        else:
+            self.name = name
